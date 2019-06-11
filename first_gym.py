35dd3891cf0568385f86727770bde86d8ec3ed02
teodorkasap <erol.erten@gmail.com>
2019-06-11 23:10:50 +0300

    first_gym.py complete

--- first_gym.py
+++ first_gym.py
@@ -4,8 +4,10 @@ env = gym.make("MsPacman-v0")
 s = env.reset()
 done = False
 
+print(env.observation_space)
+
 while not done:
     env.render()
-
+    
     a = env.action_space.sample()
     s, r, done, info = env.step(a)
